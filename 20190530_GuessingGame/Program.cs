5fa5c899f320e0d63cc5cfab5911dc7fdb6402a7
Artemenko Art <artemenkoart82@gmail.com>
2019-05-30 19:20:56 +0300

    First commit

---
+++ 20190530_GuessingGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190530_GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //regular player
+            //player notepad
+            //Uber player
+            //Cheater
+            //Uber-Cheater
+
+        }
+    }
+}
